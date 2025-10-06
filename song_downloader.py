29b4d38b7d576d2f0e1fbaae02a765676c3f88f7
bentaffet <btaffet@wesleyan.edu>
2025-10-06 14:21:37 -0400

    trying github pages

--- song_downloader.py
+++ song_downloader.py
@@ -3,6 +3,18 @@ import os
 import sys
 
 
+PLAYLIST_LINK_LIST = [('https://open.spotify.com/playlist/31LVuXlRYRVq4Z6krWGedS?si=1c576d71637244b3', '60s'),
+                      ('https://open.spotify.com/playlist/5KmBulox9POMt9hOt3VV1x?si=91bb0f2504a642a9', '70s'),
+                      ('https://open.spotify.com/playlist/19PgP2QSGPcm6Ve8VhbtpG?si=f0f2b905b9cb42c0', '80s'),
+                      ('https://open.spotify.com/playlist/3C64V048fGyQfCjmu9TIGA?si=7f0e3aab308e43a5', '90s'),
+                      ('https://open.spotify.com/playlist/5JBCdq5x9YbODn9XAqFY0f?si=c405adc106804ecf', '2000s'),
+                      ('https://open.spotify.com/playlist/6dzWCbiW5HjxaBMuo5THnn?si=8dba56ddea94414c', '2010s'),
+                      ('https://open.spotify.com/playlist/4vSTV61efRmetmaoz95Vet?si=be19efb6dffc4e6c', '2020s'),
+                      ('https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M?si=7e75cd8cf3654f87', 'Todays Top Hits'),
+                      ('https://open.spotify.com/playlist/6tJ7yq9VRd2vO4oBHvVKsM?pi=yMZAQpUnQKu3W', 'Will Wise Classic Rock')
+                      ]
+
+
 install_spotdl = input("Do you need to install spotdl? (y/n) ").lower()
 if install_spotdl == 'y':
     try:
@@ -21,22 +33,12 @@ if install_ffmpeg == 'y':
         print("Failed to install FFmpeg.")
         print(e)
 
-playlist_link_list = [('https://open.spotify.com/playlist/31LVuXlRYRVq4Z6krWGedS?si=1c576d71637244b3', '60s'),
-                      ('https://open.spotify.com/playlist/5KmBulox9POMt9hOt3VV1x?si=91bb0f2504a642a9', '70s'),
-                      ('https://open.spotify.com/playlist/19PgP2QSGPcm6Ve8VhbtpG?si=f0f2b905b9cb42c0', '80s'),
-                      ('https://open.spotify.com/playlist/3C64V048fGyQfCjmu9TIGA?si=7f0e3aab308e43a5', '90s'),
-                      ('https://open.spotify.com/playlist/5JBCdq5x9YbODn9XAqFY0f?si=c405adc106804ecf', '2000s'),
-                      ('https://open.spotify.com/playlist/6dzWCbiW5HjxaBMuo5THnn?si=8dba56ddea94414c', '2010s'),
-                      ('https://open.spotify.com/playlist/4vSTV61efRmetmaoz95Vet?si=be19efb6dffc4e6c', '2020s'),
-                      ('https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M?si=7e75cd8cf3654f87', 'Todays Top Hits'),
-                      ('https://open.spotify.com/playlist/6tJ7yq9VRd2vO4oBHvVKsM?pi=yMZAQpUnQKu3W', 'Will Wise Classic Rock')
-                      ]
 
 current_directory = os.getcwd()
 
 subprocess.run(["mkdir", "songs"])
 
-for item in playlist_link_list:
+for item in PLAYLIST_LINK_LIST:
     link, playlist_name = item
 
     output_directory = os.path.join(current_directory, "songs", playlist_name)
